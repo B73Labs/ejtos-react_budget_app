8f28d5591c4e0862fab64fb3e75acdb9f29fe0a0
Michael Best <michael.best@gmail.com>
2023-01-22 19:55:34 -0500

    Initial changes to Budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
+    const [budget, setBudget] = useState('');
 
+    function handleChange(e) {
+        e.preventDefault();
+
+        if(budget < expenses) {
+            alert("The budget cannot be less than the existing expenses of £"+expenses);
+            return;
+        }
+        let maxBudget = 20000;
+        if(budget > maxBudget) {
+            alert("The budget cannot exceed £"+maxBudget);
+            return;
+        }
+        console.log(budget.value);
+        setBudget(budget.value);
+
+      }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <form onChange={handleChange}>
+                    <input
+                    type="number"
+                    onChange={(e) => handleChange(e.target.value)}
+                    value={budget}
+                    step="10"
+                    />
+                </form>
+          </span>
         </div>
     );
 };
